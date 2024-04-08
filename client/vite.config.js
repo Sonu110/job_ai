6d4483bd103b27d1c7ee54dcf61024db90245abe
Sonu110 <kumarsonu60067@gmail.com>
2024-04-08 16:15:38 +0530

    vite  update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+import { VitePWA } from 'vite-plugin-pwa'
 
 
 
@@ -8,6 +8,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()
- 
+  ,
+  VitePWA()
   ],
 })
